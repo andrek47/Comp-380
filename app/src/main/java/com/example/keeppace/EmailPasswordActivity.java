2d591e47ef742bb0cdf82ce3df9ff509f9262462
Nhan <dinhnt9411@student.laccd.edu>
2025-11-01 15:40:56 -0700

    x

--- app/src/main/java/com/example/keeppace/EmailPasswordActivity.java
+++ app/src/main/java/com/example/keeppace/EmailPasswordActivity.java
@@ -16,10 +16,12 @@
 
 package com.example.keeppace;
 
-import android.app.Activity;
+//import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -27,6 +29,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -38,17 +42,39 @@ public class EmailPasswordActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     // [END declare_auth]
 
+    private FirebaseAnalytics firebaseAnalytics;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email_password); // connects to the XML layout
 
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
         // [START initialize_auth]
         // Initialize Firebase Auth
+        FirebaseApp.initializeApp(this);
         mAuth = FirebaseAuth.getInstance();
         // [END initialize_auth]
         Button backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(v -> finish());
+
+        EditText emailField = findViewById(R.id.fieldEmail);
+        EditText passwordField = findViewById(R.id.fieldPassword);
+
+        Button emailCreateAccountButton = findViewById(R.id.emailCreateAccountButton);
+        emailCreateAccountButton.setOnClickListener(view -> {
+            String email = emailField.getText().toString().trim();
+            String password = passwordField.getText().toString().trim();
+            createAccount(email, password);
+        });
+
+        Button emailSignInButton = findViewById(R.id.emailSignInButton);
+        emailSignInButton.setOnClickListener(view -> {
+            String email = emailField.getText().toString().trim();
+            String password = passwordField.getText().toString().trim();
+            signIn(email, password);
+        });
     }
 
     // [START on_start_check_user]
@@ -66,21 +92,19 @@ public class EmailPasswordActivity extends AppCompatActivity {
     private void createAccount(String email, String password) {
         // [START create_user_with_email]
         mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user);
+                        sendEmailVerification();
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                        updateUI(null);
                     }
                 });
         // [END create_user_with_email]
@@ -97,6 +121,17 @@ public class EmailPasswordActivity extends AppCompatActivity {
                             Log.d(TAG, "signInWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
                             updateUI(user);
+
+                            Bundle bundle = new Bundle();
+                            bundle.putString(FirebaseAnalytics.Param.METHOD, "button_click");
+                            firebaseAnalytics.logEvent("test_firebase_event", bundle);
+                            Toast.makeText(EmailPasswordActivity.this, "Firebase event logged!", Toast.LENGTH_SHORT).show();
+
+                            if(user != null) {
+                                Intent intent = new Intent(EmailPasswordActivity.this, HomePage.class);
+                                startActivity(intent);
+                                finish();
+                            }
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
