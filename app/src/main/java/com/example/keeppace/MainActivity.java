55fc958af51dfb73c31e048f13b62cfbbab43bdc
Nhan <dinhnt9411@student.laccd.edu>
2025-11-01 17:21:37 -0700

    new addons and changes to user creation

--- app/src/main/java/com/example/keeppace/MainActivity.java
+++ app/src/main/java/com/example/keeppace/MainActivity.java
@@ -1,9 +1,21 @@
 package com.example.keeppace; // make sure this matches your package name
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
@@ -25,6 +37,31 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this,HomePage.class);
             startActivity(intent);
         } );*/
+
+        TextView signUpText = findViewById(R.id.signUpText);
+
+        String text = "New To Keep Pace? Sign Up";
+        SpannableString spannable = new SpannableString(text);
+
+        ClickableSpan signUpClick = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                Intent intent = new Intent(MainActivity.this, EmailPasswordActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        int start = text.indexOf("Sign Up");
+        int end = start + "Sign Up".length();
+
+        spannable.setSpan(signUpClick, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannable.setSpan(new ForegroundColorSpan(Color.BLUE), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannable.setSpan(new StyleSpan(Typeface.BOLD), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        signUpText.setText(spannable);
+        signUpText.setMovementMethod(LinkMovementMethod.getInstance());
+        signUpText.setHighlightColor(Color.TRANSPARENT);
+
         goToAuth.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, EmailPasswordActivity.class);
             startActivity(intent);
