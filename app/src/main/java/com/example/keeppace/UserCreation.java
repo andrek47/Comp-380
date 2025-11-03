6b4b9a97af25c2b54b9c3aba926becc0651ce85a
Nhan <dinhnt9411@student.laccd.edu>
2025-11-02 19:08:00 -0800

    new addons and made a new page to usercreation also now you can make a new user

--- app/src/main/java/com/example/keeppace/UserCreation.java
+++ app/src/main/java/com/example/keeppace/UserCreation.java
@@ -7,14 +7,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-//import com.google.firebase.FirebaseApp;
-//import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.auth.AuthResult;
+
+
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -65,28 +62,25 @@ public class UserCreation extends AppCompatActivity {
     private void createAccount(String email, String password) {
         // [START create_user_with_email]
         mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(UserCreation.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
+                .addOnCompleteListener(UserCreation.this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user);
 
-                            if (user != null) {
-                                user.sendEmailVerification();
-                                Intent intent = new Intent(UserCreation.this, HomePage.class);
-                                startActivity(intent);
-                                finish();
-                            }
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(UserCreation.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
+                        if (user != null) {
+                            user.sendEmailVerification();
+                            Intent intent = new Intent(UserCreation.this, HomePage.class);
+                            startActivity(intent);
+                            finish();
                         }
+                    } else {
+                        // If sign up fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(UserCreation.this, "User creation failed.",
+                                Toast.LENGTH_SHORT).show();
+                        updateUI(null);
                     }
                 });
 
@@ -95,7 +89,7 @@ public class UserCreation extends AppCompatActivity {
         // [END create_user_with_email]
 
 
-    private void sendEmailVerification() {
+    /* private void sendEmailVerification() {
         // Send verification email
         // [START send_email_verification]
         final FirebaseUser user = mAuth.getCurrentUser();
@@ -108,9 +102,8 @@ public class UserCreation extends AppCompatActivity {
                 });
         // [END send_email_verification]
     }
+*/
 
-    private void reload() {
-    }
 
     private void updateUI(FirebaseUser user) {
         if(user != null){
