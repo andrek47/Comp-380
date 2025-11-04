7ca60484791ef9d6706fa75a6f855958c0345518
Nhan <dinhnt9411@student.laccd.edu>
2025-11-03 16:30:35 -0800

    changed the back button to sign out button

--- app/src/main/java/com/comp380/keeppace/HomePage.java
+++ app/src/main/java/com/comp380/keeppace/HomePage.java
@@ -1,8 +1,14 @@
 package com.comp380.keeppace;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class HomePage extends AppCompatActivity {
@@ -15,6 +21,13 @@ public class HomePage extends AppCompatActivity {
         setContentView(R.layout.activity_homepage);
 
         Button backButton = findViewById(R.id.backButton);
-        backButton.setOnClickListener(v -> finish());
+        backButton.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            Toast.makeText(this, "You have signed out.", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(HomePage.this, EmailPasswordActivity.class);
+            startActivity(intent);
+        });
+
+
     }
 }
