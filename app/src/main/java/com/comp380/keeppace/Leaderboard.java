cf98aa848e229509ced628dcd117f15f70af3607
Andre <andrekalay@gmail.com>
2025-11-07 19:57:48 -0800

    Added Leaderboard UI

--- app/src/main/java/com/comp380/keeppace/Leaderboard.java
+++ app/src/main/java/com/comp380/keeppace/Leaderboard.java
@@ -27,10 +27,43 @@ public class Leaderboard extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView( LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the layout
+        View view = inflater.inflate(R.layout.fragment_leaderboard, container, false);
 
-        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
+        // Example: manually setting text in the table
+        TextView name1 = view.findViewById(R.id.name1);
+        TextView score1 = view.findViewById(R.id.score1);
+        TextView rank1 = view.findViewById(R.id.rank1);
+
+        TextView name2 = view.findViewById(R.id.name2);
+        TextView score2 = view.findViewById(R.id.score2);
+        TextView rank2 = view.findViewById(R.id.rank2);
+
+        // Set text manually for testing
+        name1.setText("Andre");
+        score1.setText("1000000");
+        rank1.setText("1");
+
+        name2.setText("test");
+        score2.setText("0");
+        rank2.setText("2");
+
+
+
+
+        TextView txtFirst = view.findViewById(R.id.txt_first);
+        TextView txtSecond = view.findViewById(R.id.txt_second);
+        TextView txtThird = view.findViewById(R.id.txt_third);
+
+        txtFirst.setText("Andre");
+        txtSecond.setText("Test");
+
+
+
+
+        return view;
     }
 }
